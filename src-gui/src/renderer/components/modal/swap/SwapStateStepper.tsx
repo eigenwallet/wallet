4cb73d05a6990ba01ab4009a31354e0dc742b52b
binarybaron <binarybaron@unstoppableswap.net>
2024-09-11 17:51:35 +0200

    refactor(gui): Rework swap state stepper to use Tauri events

--- src-gui/src/renderer/components/modal/swap/SwapStateStepper.tsx
+++ src-gui/src/renderer/components/modal/swap/SwapStateStepper.tsx
@@ -1,11 +1,7 @@
 import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
 import { SwapSpawnType } from "models/cliModel";
-import { BobStateName } from "models/tauriModelExt";
-import {
-  useActiveSwapInfo,
-  useAppSelector,
-  useIsSwapRunning,
-} from "store/hooks";
+import { SwapSlice } from "models/storeModel";
+import { useAppSelector } from "store/hooks";
 import { exhaustiveGuard } from "utils/typescriptUtils";
 
 export enum PathType {
@@ -13,68 +9,102 @@ export enum PathType {
   UNHAPPY_PATH = "unhappy path",
 }
 
-// TODO: Consider using a TauriProgressEvent here instead of BobStateName
-// TauriProgressEvent is always up to date, BobStateName is not (needs to be periodically fetched)
+/**
+ * Determines the current step in the swap process based on the previous and latest state.
+ * @param prevState - The previous state of the swap process (null if it's the initial state)
+ * @param latestState - The latest state of the swap process
+ * @returns A tuple containing [PathType, activeStep, errorFlag]
+ */
 function getActiveStep(
-  stateName: BobStateName | null,
-  processExited: boolean,
-): [PathType, number, boolean] {
-  switch (stateName) {
-    /// // Happy Path
-    // Step: 0 (Waiting for Bitcoin lock tx to be published)
-    case null:
-      return [PathType.HAPPY_PATH, 0, false];
-    case BobStateName.Started:
-    case BobStateName.SwapSetupCompleted:
+  state: SwapSlice["state"],
+): [type: PathType, step: number, isError: boolean] {
+  if (state === null) {
+    return [PathType.HAPPY_PATH, 0, false];
+  }
+
+  const prevState = state.prev;
+  let latestState = state.curr;
+
+  const processExited = latestState.type === "Released";
+
+  // If the swap is completed we use the previous state to display the correct step
+  if (latestState.type === "Released") {
+    latestState = prevState;
+  }
+
+  switch (latestState.type) {
+    // Step 0: Initializing the swap
+    // These states represent the very beginning of the swap process
+    // No funds have been locked
+    case "Initiated":
+    case "ReceivedQuote":
+    case "WaitingForBtcDeposit":
+    case "Started":
       return [PathType.HAPPY_PATH, 0, processExited];
 
-    // Step: 1 (Waiting for Bitcoin Lock confirmation and XMR Lock Publication)
-    // We have locked the Bitcoin and are waiting for the other party to lock their XMR
-    case BobStateName.BtcLocked:
+    // Step 1: Waiting for Bitcoin lock confirmation
+    // Bitcoin has been locked, waiting for the counterparty to lock their XMR
+    case "BtcLockTxInMempool":
       return [PathType.HAPPY_PATH, 1, processExited];
 
-    // Step: 2 (Waiting for XMR Lock confirmation)
-    // We have locked the Bitcoin and the other party has locked their XMR
-    case BobStateName.XmrLockProofReceived:
+    // Still Step 1: Both Bitcoin and XMR have been locked, waiting for Monero lock to be confirmed
+    case "XmrLockTxInMempool":
       return [PathType.HAPPY_PATH, 1, processExited];
 
-    // Step: 3 (Sending Encrypted Signature and waiting for Bitcoin Redemption)
-    // The XMR lock transaction has been confirmed
-    // We now need to send the encrypted signature to the other party and wait for them to redeem the Bitcoin
-    case BobStateName.XmrLocked:
-    case BobStateName.EncSigSent:
+    // Step 2: Waiting for encrypted signature to be sent to Alice
+    // and for Alice to redeem the Bitcoin
+    case "XmrLocked":
+      // case BobStateName.EncSigSent: // TODO: There is no equivalent for this with Tauri events
       return [PathType.HAPPY_PATH, 2, processExited];
 
-    // Step: 4 (Waiting for XMR Redemption)
-    case BobStateName.BtcRedeemed:
+    // Step 3: Waiting for XMR redemption
+    // Bitcoin has been redeemed by Alice, now waiting for us to redeem Monero
+    case "BtcRedeemed":
       return [PathType.HAPPY_PATH, 3, processExited];
 
-    // Step: 4 (Completed) (Swap completed, XMR redeemed)
-    case BobStateName.XmrRedeemed:
+    // Step 4: Swap completed successfully
+    // XMR redemption transaction is in mempool, swap is essentially complete
+    case "XmrRedeemInMempool":
       return [PathType.HAPPY_PATH, 4, false];
 
     // Edge Case of Happy Path where the swap is safely aborted. We "fail" at the first step.
-    case BobStateName.SafelyAborted:
-      return [PathType.HAPPY_PATH, 0, true];
+    // TODO: There's no equivalent for this with the Tauri events
+    // case BobStateName.SafelyAborted:
+    //  return [PathType.HAPPY_PATH, 0, true];
 
     // // Unhappy Path
     // Step: 1 (Cancelling swap, checking if cancel transaction has been published already by the other party)
-    case BobStateName.CancelTimelockExpired:
-      return [PathType.UNHAPPY_PATH, 0, processExited];
+    // TODO: There's no equivalent for this with the Tauri events
+    //case BobStateName.CancelTimelockExpired:
+    //  return [PathType.UNHAPPY_PATH, 0, processExited];
 
-    // Step: 2 (Attempt to publish the Bitcoin refund transaction)
-    case BobStateName.BtcCancelled:
+    // Unhappy Path States
+
+    // Step 2: Swap has been cancelled. Waiting for Bitcoin to be refunded
+    case "BtcCancelled":
       return [PathType.UNHAPPY_PATH, 1, processExited];
 
-    // Step: 2 (Completed) (Bitcoin refunded)
-    case BobStateName.BtcRefunded:
+    // Step 2: Swap cancelled and Bitcoin refunded successfully
+    case "BtcRefunded":
       return [PathType.UNHAPPY_PATH, 2, false];
 
-    // Step: 2 (We failed to publish the Bitcoin refund transaction)
-    // We failed to publish the Bitcoin refund transaction because the timelock has expired.
-    // We will be punished. Nothing we can do about it now.
-    case BobStateName.BtcPunished:
+    // Step 2 (Failed): Failed to refund Bitcoin
+    // The timelock expired before we could refund, resulting in punishment
+    case "BtcPunished":
       return [PathType.UNHAPPY_PATH, 1, true];
+
+    // Attempting cooperative redemption after punishment
+    case "AttemptingCooperativeRedeem":
+    case "CooperativeRedeemAccepted":
+      return [PathType.UNHAPPY_PATH, 1, false];
+    case "CooperativeRedeemRejected":
+      return [PathType.UNHAPPY_PATH, 1, true];
+
+    case "Released":
+      throw new Error(
+        "Unexpected, latest and previous state cannot be 'Released'",
+      );
+
     default:
       return exhaustiveGuard(stateName);
   }
@@ -147,7 +177,7 @@ function UnhappyPathStepper({
           optional={<Typography variant="caption">~20min</Typography>}
           error={error && activeStep === 1}
         >
-          Refunding your BTC
+          Attempting recovery
         </StepLabel>
       </Step>
     </Stepper>
@@ -158,9 +188,9 @@ export default function SwapStateStepper() {
   // TODO: There's no equivalent of this with Tauri yet.
   const currentSwapSpawnType = useAppSelector((s) => s.swap.spawnType);
 
-  const stateName = useActiveSwapInfo()?.state_name ?? null;
-  const processExited = !useIsSwapRunning();
-  const [pathType, activeStep, error] = getActiveStep(stateName, processExited);
+  const swapState = useAppSelector((s) => s.swap.state);
+
+  const [pathType, activeStep, error] = getActiveStep(swapState);
 
   // TODO: Fix this to work with Tauri
   // If the current swap is being manually cancelled and refund, we want to show the unhappy path even though the current state is not a "unhappy" state
