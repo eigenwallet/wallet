26da7d0e4b4be3b964678f2b2416fa61a36b591c
binarybaron <binarybaron@unstoppableswap.net>
2024-10-18 16:14:58 +0200

    fix(lip2p): Use new NetworkBehaviour API for swap/src/network/redial.rs

--- swap/src/network/redial.rs
+++ swap/src/network/redial.rs
@@ -2,11 +2,9 @@ use crate::cli;
 use backoff::backoff::Backoff;
 use backoff::ExponentialBackoff;
 use futures::future::FutureExt;
-use libp2p::core::connection::ConnectionId;
 use libp2p::core::Multiaddr;
 use libp2p::swarm::dial_opts::{DialOpts, PeerCondition};
-use libp2p::swarm::protocols_handler::DummyProtocolsHandler;
-use libp2p::swarm::{NetworkBehaviour, NetworkBehaviourAction, PollParameters};
+use libp2p::swarm::{NetworkBehaviour, ToSwarm};
 use libp2p::PeerId;
 use std::pin::Pin;
 use std::task::{Context, Poll};
@@ -38,8 +36,9 @@ impl Behaviour {
             backoff: ExponentialBackoff {
                 initial_interval: interval,
                 current_interval: interval,
-                // give up dialling after 5 minutes
-                max_elapsed_time: Some(Duration::from_secs(5 * 60)),
+                // Never give up dialing
+                // TOOD: Libp2p Upgrade: Is this the right approach?
+                max_elapsed_time: None,
                 ..ExponentialBackoff::default()
             },
         }
@@ -57,44 +56,48 @@ impl Behaviour {
 }
 
 impl NetworkBehaviour for Behaviour {
-    type ProtocolsHandler = DummyProtocolsHandler;
-    type OutEvent = OutEvent;
+    type ConnectionHandler = libp2p::swarm::dummy::ConnectionHandler;
+    type ToSwarm = OutEvent;
 
-    fn new_handler(&mut self) -> Self::ProtocolsHandler {
-        DummyProtocolsHandler::default()
-    }
-
-    fn addresses_of_peer(&mut self, _: &PeerId) -> Vec<Multiaddr> {
-        Vec::new()
-    }
-
-    fn inject_connected(&mut self, peer_id: &PeerId) {
-        if peer_id != &self.peer {
-            return;
-        }
-
-        // established a connection to the desired peer, cancel any active re-dialling
-        self.sleep = None;
-    }
-
-    fn inject_disconnected(&mut self, peer_id: &PeerId) {
-        if peer_id != &self.peer {
-            return;
-        }
-
-        // lost connection to the configured peer, trigger re-dialling with an
-        // exponential backoff
-        self.backoff.reset();
-        self.sleep = Some(Box::pin(tokio::time::sleep(self.backoff.initial_interval)));
-    }
-
-    fn inject_event(&mut self, _: PeerId, _: ConnectionId, _: Void) {}
-
-    fn poll(
+    fn handle_established_inbound_connection(
         &mut self,
-        cx: &mut Context<'_>,
-        _: &mut impl PollParameters,
-    ) -> Poll<NetworkBehaviourAction<Self::OutEvent, Self::ProtocolsHandler>> {
+        _connection_id: libp2p::swarm::ConnectionId,
+        peer: PeerId,
+        _local_addr: &Multiaddr,
+        _remote_addr: &Multiaddr,
+    ) -> Result<libp2p::swarm::THandler<Self>, libp2p::swarm::ConnectionDenied> {
+        if peer == self.peer {
+            self.sleep = None; // Cancel any active re-dialling
+        }
+        Ok(Self::ConnectionHandler::default())
+    }
+
+    fn handle_established_outbound_connection(
+        &mut self,
+        _connection_id: libp2p::swarm::ConnectionId,
+        peer: PeerId,
+        _addr: &Multiaddr,
+        _role_override: libp2p::core::Endpoint,
+    ) -> Result<libp2p::swarm::THandler<Self>, libp2p::swarm::ConnectionDenied> {
+        if peer == self.peer {
+            self.sleep = None; // Cancel any active re-dialling
+        }
+        Ok(Self::ConnectionHandler::default())
+    }
+
+    fn on_swarm_event(&mut self, event: libp2p::swarm::FromSwarm) {
+        // TODO: Libp2p Upgrade: Maybe we also need to match for FromSwarm::DialFailure here?
+
+        if let libp2p::swarm::FromSwarm::ConnectionClosed(closed) = event {
+            if closed.peer_id == self.peer {
+                // Lost connection, trigger re-dialling with exponential backoff
+                self.backoff.reset();
+                self.sleep = Some(Box::pin(tokio::time::sleep(self.backoff.initial_interval)));
+            }
+        }
+    }
+
+    fn poll(&mut self, cx: &mut Context<'_>) -> std::task::Poll<ToSwarm<Self::ToSwarm, Void>> {
         let sleep = match self.sleep.as_mut() {
             None => return Poll::Pending, // early exit if we shouldn't be re-dialling
             Some(future) => future,
@@ -105,20 +108,29 @@ impl NetworkBehaviour for Behaviour {
         let next_dial_in = match self.backoff.next_backoff() {
             Some(next_dial_in) => next_dial_in,
             None => {
-                return Poll::Ready(NetworkBehaviourAction::GenerateEvent(
-                    OutEvent::AllAttemptsExhausted { peer: self.peer },
-                ));
+                return Poll::Ready(ToSwarm::GenerateEvent(OutEvent::AllAttemptsExhausted {
+                    peer: self.peer,
+                }));
             }
         };
 
         self.sleep = Some(Box::pin(tokio::time::sleep(next_dial_in)));
 
-        Poll::Ready(NetworkBehaviourAction::Dial {
+        Poll::Ready(ToSwarm::Dial {
             opts: DialOpts::peer_id(self.peer)
                 .condition(PeerCondition::Disconnected)
                 .build(),
-            handler: Self::ProtocolsHandler::default(),
         })
+    }
+
+    fn on_connection_handler_event(
+        &mut self,
+        _peer_id: PeerId,
+        _connection_id: libp2p::swarm::ConnectionId,
+        _event: libp2p::swarm::THandlerOutEvent<Self>,
+    ) {
+        // the dummy connection handler does not produce any events
+        // therefore we do not need to handle any events here
     }
 }
 
