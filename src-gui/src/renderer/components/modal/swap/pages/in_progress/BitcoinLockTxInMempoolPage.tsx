65786ae18b7b1ea08d33ce10db22717f1e2807d2
Binarybaron <binarybaron@protonmail.com>
2024-11-14 12:58:55 +0100

    remove redundant imports

--- src-gui/src/renderer/components/modal/swap/pages/in_progress/BitcoinLockTxInMempoolPage.tsx
+++ src-gui/src/renderer/components/modal/swap/pages/in_progress/BitcoinLockTxInMempoolPage.tsx
@@ -1,9 +1,8 @@
-timport { Box, DialogContentText } from "@material-ui/core";
 import { TauriSwapProgressEventContent } from "models/tauriModelExt";
-import SwapMightBeCancelledAlert from "../../../../alert/SwapMightBeCancelledAlert";
 import BitcoinTransactionInfoBox from "../../BitcoinTransactionInfoBox";
 import SwapStatusAlert from "renderer/components/alert/SwapStatusAlert/SwapStatusAlert";
 import { useActiveSwapInfo } from "store/hooks";
+import { Box, DialogContentText } from "@material-ui/core";
 
 // This is the number of blocks after which we consider the swap to be at risk of being unsuccessful
 const BITCOIN_CONFIRMATIONS_WARNING_THRESHOLD = 2;
