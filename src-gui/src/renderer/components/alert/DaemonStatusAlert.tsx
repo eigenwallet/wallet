0699fdebb63a319e27248285a597ef2bc9d1a400
Binarybaron <binarybaron@protonmail.com>
2024-11-21 01:14:54 +0100

    feat(gui): Initialize wallets, tor client concurrently

--- src-gui/src/renderer/components/alert/DaemonStatusAlert.tsx
+++ src-gui/src/renderer/components/alert/DaemonStatusAlert.tsx
@@ -1,6 +1,5 @@
 import { Box, Button, LinearProgress, makeStyles } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
-import { TauriContextInitializationProgress } from "models/tauriModel";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "store/hooks";
 import { exhaustiveGuard } from "utils/typescriptUtils";
@@ -20,51 +19,60 @@ export default function DaemonStatusAlert() {
   const contextStatus = useAppSelector((s) => s.rpc.status);
   const navigate = useNavigate();
 
-  if (contextStatus === null) {
+  if (contextStatus === null || contextStatus.type === "NotInitialized") {
     return <LoadingSpinnerAlert severity="warning">Checking for available remote nodes</LoadingSpinnerAlert>;
   }
 
   switch (contextStatus.type) {
     case "Initializing":
-      switch (contextStatus.content.type) {
-        case "OpeningBitcoinWallet":
-          return (
-            <LoadingSpinnerAlert severity="warning">
-              Connecting to the Bitcoin network
-            </LoadingSpinnerAlert>
-          );
-        case "DownloadingMoneroWalletRpc":
-          return (
-            <LoadingSpinnerAlert severity="warning">
-              <Box className={classes.innerAlert}>
-                <Box>
-                  Downloading and verifying the Monero wallet RPC (
-                  {bytesToMb(contextStatus.content.content.size).toFixed(2)} MB)
-                </Box>
-                <LinearProgress variant="determinate" value={contextStatus.content.content.progress} />
-              </Box>
-            </LoadingSpinnerAlert >
-          );
-        case "OpeningMoneroWallet":
-          return (
-            <LoadingSpinnerAlert severity="warning">
-              Connecting to the Monero network
-            </LoadingSpinnerAlert>
-          );
-        case "OpeningDatabase":
-          return (
-            <LoadingSpinnerAlert severity="warning">
-              Opening the local database
-            </LoadingSpinnerAlert>
-          );
-        case "EstablishingTorCircuits":
-          return (
-            <LoadingSpinnerAlert severity="warning">
-              Connecting to the Tor network
-            </LoadingSpinnerAlert>
-          );
-      }
-      break;
+      return contextStatus.content
+        .map((status) => {
+          if (status.progress.type === "Completed") {
+            return null;
+          }
+
+          switch (status.componentName) {
+            case "OpeningBitcoinWallet":
+              return (
+                <LoadingSpinnerAlert severity="warning">
+                  Syncing internal Bitcoin wallet
+                </LoadingSpinnerAlert>
+              );
+            case "DownloadingMoneroWalletRpc":
+              return (
+                <LoadingSpinnerAlert severity="warning">
+                  <Box className={classes.innerAlert}>
+                    <Box>
+                      Downloading and verifying the Monero wallet RPC (
+                      {bytesToMb(status.progress.content.size).toFixed(2)} MB)
+                    </Box>
+                    <LinearProgress variant="determinate" value={status.progress.content.progress} />
+                  </Box>
+                </LoadingSpinnerAlert >
+              );
+            case "OpeningMoneroWallet":
+              return (
+                <LoadingSpinnerAlert severity="warning">
+                  Opening the Monero wallet
+                </LoadingSpinnerAlert>
+              );
+            case "OpeningDatabase":
+              return (
+                <LoadingSpinnerAlert severity="warning">
+                  Opening the local database
+                </LoadingSpinnerAlert>
+              );
+            case "EstablishingTorCircuits":
+              return (
+                <LoadingSpinnerAlert severity="warning">
+                  Establishing Tor circuits
+                </LoadingSpinnerAlert>
+              )
+            default:
+              return exhaustiveGuard(status.componentName);
+          }
+        })
+        .filter((s) => s !== null);
     case "Available":
       return <Alert severity="success">The daemon is running</Alert>;
     case "Failed":
