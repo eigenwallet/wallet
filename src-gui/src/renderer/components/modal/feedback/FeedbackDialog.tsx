7b43ce0475960760999c3a9e22d0906b4eb61d99
b-enedict <benedict.seuss@gmail.com>
2025-05-20 09:18:24 +0200

    refactor(gui): Enhance FeedbackDialog layout and add mail link

--- src-gui/src/renderer/components/modal/feedback/FeedbackDialog.tsx
+++ src-gui/src/renderer/components/modal/feedback/FeedbackDialog.tsx
@@ -14,7 +14,6 @@ import {
     Typography,
 } from '@material-ui/core'
 import { Visibility } from '@material-ui/icons'
-import LoadingButton from '../../other/LoadingButton'
 import ExternalLink from 'renderer/components/other/ExternalLink'
 import SwapSelectDropDown from './SwapSelectDropDown'
 import LogViewer from './LogViewer'
@@ -44,137 +43,152 @@ export default function FeedbackDialog({
 
     return (
         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Submit Feedback</DialogTitle>
+            <DialogTitle style={{ paddingBottom: '0.5rem' }}>
+                Submit Feedback
+            </DialogTitle>
             <DialogContent>
-                <Typography>
-                    Have a question or need assistance? Message us below or{' '}
-                    <ExternalLink href="#">email us</ExternalLink>!
-                </Typography>
                 <Box
                     style={{
                         display: 'flex',
                         flexDirection: 'column',
-                        gap: '1rem',
+                        gap: '1.5rem',
                     }}
                 >
-                    <TextField
-                        variant="outlined"
-                        value={input.bodyText}
-                        onChange={(e) =>
-                            setInputState((prev) => ({
-                                ...prev,
-                                bodyText: e.target.value,
-                            }))
-                        }
-                        label={
-                            bodyTooLong
-                                ? `Text is too long (${input.bodyText.length}/${MAX_FEEDBACK_LENGTH})`
-                                : 'Message'
-                        }
-                        multiline
-                        minRows={4}
-                        maxRows={4}
-                        fullWidth
-                        error={bodyTooLong}
-                    />
-                    <Typography>
-                        Attach logs with your feedback for better support.
-                    </Typography>
-                    <Box
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            gap: '1rem',
-                        }}
-                    >
-                        <SwapSelectDropDown
-                            selectedSwap={input.selectedSwap}
-                            setSelectedSwap={(swapId) =>
+                    <Box>
+                        <Typography style={{ marginBottom: '0.5rem' }}>
+                            Have a question or need assistance? Message us below
+                            or <ExternalLink href="mailto:help@unstoppableswap.net">email us</ExternalLink>!
+                        </Typography>
+                        <TextField
+                            variant="outlined"
+                            value={input.bodyText}
+                            onChange={(e) =>
                                 setInputState((prev) => ({
                                     ...prev,
-                                    selectedSwap: swapId,
+                                    bodyText: e.target.value,
                                 }))
                             }
+                            label={
+                                bodyTooLong
+                                    ? `Text is too long (${input.bodyText.length}/${MAX_FEEDBACK_LENGTH})`
+                                    : 'Message'
+                            }
+                            multiline
+                            minRows={4}
+                            maxRows={4}
+                            fullWidth
+                            error={bodyTooLong}
                         />
-                        <Tooltip title="View the logs">
-                            <Box
-                                style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                }}
-                            >
-                                <IconButton
-                                    onClick={() => setSwapLogsEditorOpen(true)}
-                                    disabled={input.selectedSwap === null}
-                                >
-                                    <Visibility />
-                                </IconButton>
-                            </Box>
-                        </Tooltip>
                     </Box>
-                    <LogViewer
-                        open={swapLogsEditorOpen}
-                        setOpen={setSwapLogsEditorOpen}
-                        logs={logs.swapLogs}
-                        setIsRedacted={(redact) =>
-                            setInputState((prev) => ({
-                                ...prev,
-                                isSwapLogsRedacted: redact,
-                            }))
-                        }
-                        isRedacted={input.isSwapLogsRedacted}
-                    />
-                    <Box
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            gap: '1rem',
-                        }}
-                    >
-                        <Paper
-                            variant="outlined"
-                            style={{ padding: '0.5rem', width: '100%' }}
+                    <Box>
+                        <Typography style={{ marginBottom: '0.5rem' }}>
+                            Attach logs with your feedback for better support.
+                        </Typography>
+                        <Box
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                                gap: '1rem',
+                                paddingBottom: '0.5rem',
+                            }}
                         >
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        color="primary"
-                                        checked={input.attachDaemonLogs}
-                                        onChange={(e) =>
-                                            setInputState((prev) => ({
-                                                ...prev,
-                                                attachDaemonLogs:
-                                                    e.target.checked,
-                                            }))
-                                        }
-                                    />
+                            <SwapSelectDropDown
+                                selectedSwap={input.selectedSwap}
+                                setSelectedSwap={(swapId) =>
+                                    setInputState((prev) => ({
+                                        ...prev,
+                                        selectedSwap: swapId,
+                                    }))
                                 }
-                                label="Attach logs from the current session"
                             />
-                        </Paper>
-                        <Tooltip title="View the logs">
-                            <Box
-                                style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                }}
-                            >
-                                <IconButton
-                                    onClick={() =>
-                                        setDaemonLogsEditorOpen(true)
-                                    }
-                                    disabled={input.attachDaemonLogs === false}
+                            <Tooltip title="View the logs">
+                                <Box
+                                    style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                    }}
                                 >
-                                    <Visibility />
-                                </IconButton>
-                            </Box>
-                        </Tooltip>
+                                    <IconButton
+                                        onClick={() =>
+                                            setSwapLogsEditorOpen(true)
+                                        }
+                                        disabled={input.selectedSwap === null}
+                                    >
+                                        <Visibility />
+                                    </IconButton>
+                                </Box>
+                            </Tooltip>
+                        </Box>
+                        <LogViewer
+                            open={swapLogsEditorOpen}
+                            setOpen={setSwapLogsEditorOpen}
+                            logs={logs.swapLogs}
+                            setIsRedacted={(redact) =>
+                                setInputState((prev) => ({
+                                    ...prev,
+                                    isSwapLogsRedacted: redact,
+                                }))
+                            }
+                            isRedacted={input.isSwapLogsRedacted}
+                        />
+                        <Box
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                                gap: '1rem',
+                            }}
+                        >
+                            <Paper
+                                variant="outlined"
+                                style={{ padding: '0.5rem', width: '100%' }}
+                            >
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            color="primary"
+                                            checked={input.attachDaemonLogs}
+                                            onChange={(e) =>
+                                                setInputState((prev) => ({
+                                                    ...prev,
+                                                    attachDaemonLogs:
+                                                        e.target.checked,
+                                                }))
+                                            }
+                                        />
+                                    }
+                                    label="Attach logs from the current session"
+                                />
+                            </Paper>
+                            <Tooltip title="View the logs">
+                                <Box
+                                    style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <IconButton
+                                        onClick={() =>
+                                            setDaemonLogsEditorOpen(true)
+                                        }
+                                        disabled={
+                                            input.attachDaemonLogs === false
+                                        }
+                                    >
+                                        <Visibility />
+                                    </IconButton>
+                                </Box>
+                            </Tooltip>
+                        </Box>
                     </Box>
-                    <Typography>
+                    <Typography
+                        variant="caption"
+                        color="textSecondary"
+                        style={{ marginBottom: '0.5rem' }}
+                    >
                         Your feedback will be answered in the app and can be
                         found in the Feedback tab
                     </Typography>
