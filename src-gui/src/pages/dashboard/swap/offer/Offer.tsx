9835181de53145e1933329910c8bae0d1083c96e
b-enedict <benedict.seuss@gmail.com>
2025-06-16 17:55:04 +0200

    feat(gui): Add real numbers to offer step

--- src-gui/src/pages/dashboard/swap/offer/Offer.tsx
+++ src-gui/src/pages/dashboard/swap/offer/Offer.tsx
@@ -6,11 +6,15 @@ import {
   Box,
   Divider,
   IconButton,
+  CircularProgress,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import MakerOfferItem from "../components/MakerOfferItem";
 import { useState } from "react";
+import { usePendingLockBitcoinApproval } from "store/hooks";
+import { useActiveSwapId } from "store/hooks";
+import { LockBitcoinDetails } from "models/tauriModel";
 
 export default function Offer({
   onBack,
@@ -20,14 +24,27 @@ export default function Offer({
   onNext: () => void;
 }) {
   const [feeExpanded, setFeeExpanded] = useState(false);
+
+  const approvals = usePendingLockBitcoinApproval();
+  const activeSwapId = useActiveSwapId();
+
+  const request = approvals?.find(
+    (r) => r.content.details.content.swap_id === activeSwapId,
+  );
+
+  const { btc_lock_amount, xmr_receive_amount, btc_network_fee} = (request?.content.details.content as LockBitcoinDetails) || {
+    btc_lock_amount: 0,
+    xmr_receive_amount: 0,
+    btc_network_fee: 0,
+  };
+
   return (
     <>
       <DialogContent>
         <Typography variant="body1">
           Confirm your offer to start the swap
         </Typography>
-        <MakerOfferItem />
-
+        {request ? (
         <Box
           sx={{
             display: "grid",
@@ -38,7 +55,7 @@ export default function Offer({
         >
           <Typography variant="body1">You send</Typography>
           <Typography sx={{ textAlign: "right" }} variant="body1">
-            0.00002 BTC
+            {btc_lock_amount} BTC
           </Typography>
           <Divider sx={{ gridColumn: "span 2" }} />
 
@@ -52,38 +69,32 @@ export default function Offer({
             </IconButton>
           </Box>
           <Typography sx={{ textAlign: "right" }} variant="body1">
-            0.00002 BTC
+            {btc_network_fee} BTC
           </Typography>
           {feeExpanded && (
             <>
-              <Typography variant="body2">Bitcoin Transaction Fee</Typography>
+              <Typography variant="body2">Network Fee</Typography>
               <Typography sx={{ textAlign: "right" }} variant="body2">
-                0.00002 BTC
+                {request.content.details.content.btc_network_fee} BTC
               </Typography>
 
-              <Typography variant="body2">Exchange Fee</Typography>
+              {/* <Typography variant="body2">Developer Tax</Typography>
               <Typography sx={{ textAlign: "right" }} variant="body2">
-                0.00002 BTC
-              </Typography>
-
-              <Typography variant="body2">Monero Transaction Fee</Typography>
-              <Typography sx={{ textAlign: "right" }} variant="body2">
-                0.00002 XMR
-              </Typography>
-
-              <Typography variant="body2">Developer Tax</Typography>
-              <Typography sx={{ textAlign: "right" }} variant="body2">
-                0.00002 XMR
-              </Typography>
+                {request.content.details.content.developer_tax} BTC
+              </Typography> */}
             </>
           )}
           <Divider sx={{ gridColumn: "span 2" }} />
 
           <Typography variant="body1">You receive</Typography>
           <Typography sx={{ textAlign: "right" }} variant="body1">
-            0.00002 XMR
+            {xmr_receive_amount} XMR
           </Typography>
         </Box>
+        ) : (
+          <CircularProgress />
+        )}
+
       </DialogContent>
       <DialogActions>
         <Button variant="outlined" onClick={onBack}>
