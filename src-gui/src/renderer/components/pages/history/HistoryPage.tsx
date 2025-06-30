d72ebc373ef9b52199e8813b883351948bd4be30
Binarybaron <binarybaron@protonmail.com>
2025-06-30 14:53:52 +0200

    refactor(gui): Don't display dialog on History page

--- src-gui/src/renderer/components/pages/history/HistoryPage.tsx
+++ src-gui/src/renderer/components/pages/history/HistoryPage.tsx
@@ -5,14 +5,11 @@ import SwapDialog from "../../modal/swap/SwapDialog";
 import HistoryTable from "./table/HistoryTable";
 
 export default function HistoryPage() {
-  const showDialog = useAppSelector((state) => state.swap.state !== null);
-
   return (
     <>
       <Typography variant="h3">History</Typography>
       <SwapTxLockAlertsBox />
       <HistoryTable />
-      <SwapDialog open={showDialog} onClose={() => {}} />
     </>
   );
 }
